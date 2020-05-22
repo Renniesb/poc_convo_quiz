d98c405d72bcc69975bc35c032081e4c0fbab5a2
Rennie Bevineau <rsbevineau@gmail.com>
2020-05-22 07:41:23 -0400

    add check for correct answer and routing method

--- src/App.js
+++ src/App.js
@@ -7,6 +7,24 @@ class App extends React.Component {
   state = {inputs:[] }
   handleOnChange = e => {
      this.setState({[e.target.name]:e.target.value})
+     console.log("target value")
+  }
+  handleSubmit(question){
+      let userAnswer = ""
+      question.blanks.forEach((blank) => {
+         userAnswer += this.state[blank].trim().toLowerCase()
+      })
+      console.log(userAnswer);
+
+      if(userAnswer===question.answers){
+        console.log("correct");
+      }
+      else{
+        console.log("incorrect")
+      }
+  }
+  handleNextQuestion(history){
+    history.push('/question/2')
   }
   render() { 
     return ( 
@@ -14,9 +32,9 @@ class App extends React.Component {
       <Route
             path='/question/:questionId'
             render={({ 
-              match 
+              match,history 
           }) => (
-              <ResponseText track={this.trackInputs} change={this.handleOnChange} match={match} />
+              <ResponseText submitAnswers={(question)=>{this.handleSubmit(question)}} change={this.handleOnChange} match={match} history={history} nextQuestion={this.handleNextQuestion} />
           )} 
           />
     </div>
--- src/ResponseText.js
+++ src/ResponseText.js
@@ -12,33 +12,34 @@ export default function ResponseText(props) {
     )
     return (
     
-      <section class="question-screen">  
+      <section className="question-screen">  
       <div style={{gridArea: 'header', border: '1px solid #ddd', padding: '30px'}}>
         <h4>POC Conversational Quiz</h4>
-        <header class="question-info">
+        <header className="question-info">
           <h3>Question 1 of 5</h3>
-          <h5 class="score">0 correct, 0 incorrect</h5>
+          <h5 className="score">0 correct, 0 incorrect</h5>
           <hr/>
-        <p>
-        {
-          parse(question.responseText, {
-            replace: domNode => {
-              if (domNode.name === 'input') {
-                return (
-                  <input {...domNode.attribs} onChange={props.change}/>
-                );
+          
+          {
+            parse(question.responseText, {
+              replace: domNode => {
+                if (domNode.name === 'input') {
+                  return (
+                    <input {...domNode.attribs} onChange={props.change}/>
+                  );
+                }   
               }   
-            }   
-          })
-        }
-        </p>
+            })
+          }
+          
         </header>
         <div style={{marginTop: "10px"}}
          className="feedback"></div>
         <div className="correct-answer"></div>
-        <button id="submit-answer" className="myButton">Submit answer</button>
-        <button id="next" className="myButton">Next</button>
+        <button onClick={() => {
+          props.submitAnswers(question)          
+        }} id="submit-answer" className="myButton">Submit answer</button>
+        <button id="next" className="myButton" onClick={()=>{props.nextQuestion(props.history)}}>Next</button>
       </div>
-      
     </section> );
 }
