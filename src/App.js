ffea1fd342f882764830cd39b9e57c94a1f90b85
Rennie Bevineau <rsbevineau@gmail.com>
2020-05-23 08:28:14 -0400

    separate components into folders

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ResponseText from './ResponseText';
+import ResponseText from './Response/ResponseText';
 import { Route } from 'react-router-dom';
 
 
 class App extends React.Component {
-  state = {inputs:[] }
+  state = {questionNum:1 }
+  constructor(props){
+    super(props)
+  }
   handleOnChange = e => {
      this.setState({[e.target.name]:e.target.value})
-     console.log("target value")
   }
-  handleSubmit(question){
+  handleSubmit = (question)=> {
       let userAnswer = ""
+
+
       question.blanks.forEach((blank) => {
+        // var text = "   ";
+        // text.trim().length == 0;
          userAnswer += this.state[blank].trim().toLowerCase()
       })
       console.log(userAnswer);
@@ -23,8 +29,11 @@ class App extends React.Component {
         console.log("incorrect")
       }
   }
-  handleNextQuestion(history){
-    history.push('/question/2')
+  handleNextQuestion = (history)=>{
+    
+    this.setState({questionNum:this.state.questionNum + 1},()=>{
+      history.push(`/question/${this.state.questionNum}` );
+    });
   }
   render() { 
     return ( 
---
+++ src/Response/ResponseText.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import questions from '../questions.js';
+import parse from 'html-react-parser';
+import './ResponseText.css';
+
+
+
+export default function ResponseText(props) {
+
+    const question = questions.find(q =>
+      q.id === props.match.params.questionId
+    )
+    return (
+    
+    <section className="question-screen">  
+      <div className="background-img" style={{gridArea: 'header', border: '1px solid #ddd', padding: '30px'}}>
+        <h4>POC Conversational Quiz</h4>
+        <header className="question-info">
+          <h3>Question 1 of 5</h3>
+          <h5 className="score">0 correct, 0 incorrect</h5>
+          <hr/>
+          
+          
+        </header>
+          
+        <section className="response-area">
+          <p>
+          {
+              parse(question.responseText, {
+                replace: domNode => {
+                  if (domNode.name === 'input') {
+                    return (
+                      <input {...domNode.attribs} onChange={props.change}/>
+                    );
+                  }   
+                }   
+              })
+            }
+          </p>
+          <div style={{marginTop: "10px"}}
+          className="feedback"></div>
+
+          <div className="correct-answer"></div>
+          <button onClick={() => {
+            props.submitAnswers(question)          
+          }} id="submit-answer" className="myButton">Submit answer</button>
+          <button id="next" className="myButton" onClick={()=>{props.nextQuestion(props.history)}}>Next</button>
+        </section>
+      </div>
+    </section> );
+}
