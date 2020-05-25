7309ac6ccb52415d1702f56985c2de0dfb32fc47
Rennie Bevineau <rsbevineau@gmail.com>
2020-05-25 18:29:56 -0400

    make changes to the title of the quiz

--- src/StartQuiz/StartQuiz.js
+++ src/StartQuiz/StartQuiz.js
@@ -6,7 +6,7 @@ function StartQuiz(){
     return <section className="start-end-screen">
                 <div className="background-img" style={{gridArea: 'header'}}>
                 <div className="start-title">
-                    <h2>Conversation Quiz</h2>
+                    <h2>Conversational Quiz</h2>
                 </div>
                 <header className="start-end-info"></header>
                 <div className="center-button"><Link id="start-quiz" className="myButton" to="/question/1">Start Quiz</Link></div>
