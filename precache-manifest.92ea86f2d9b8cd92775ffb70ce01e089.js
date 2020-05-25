b30007836a8db19e320d75a4c68ce30f32bd51d9
Rennie Bevineau <rsbevineau@gmail.com>
2020-05-25 12:17:04 -0400

    Updates

---
+++ precache-manifest.92ea86f2d9b8cd92775ffb70ce01e089.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a562f62b60acfdd06520f2d4cc6efc45",
+    "url": "/poc_convo_quiz/index.html"
+  },
+  {
+    "revision": "01a43f4fd8150d282f34",
+    "url": "/poc_convo_quiz/static/css/main.65e8d38d.chunk.css"
+  },
+  {
+    "revision": "4b56de4c57c6f6e39670",
+    "url": "/poc_convo_quiz/static/js/2.0b4a40b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/poc_convo_quiz/static/js/2.0b4a40b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a43f4fd8150d282f34",
+    "url": "/poc_convo_quiz/static/js/main.2a8c0ec5.chunk.js"
+  },
+  {
+    "revision": "e3460bb486369723e784",
+    "url": "/poc_convo_quiz/static/js/runtime-main.b3812943.js"
+  },
+  {
+    "revision": "00419f3d6868586170df514bd24756e4",
+    "url": "/poc_convo_quiz/static/media/family1.00419f3d.mp3"
+  },
+  {
+    "revision": "7a5f15ac960d8bdde9707ebbfe1b225a",
+    "url": "/poc_convo_quiz/static/media/family10.7a5f15ac.mp3"
+  },
+  {
+    "revision": "7f07c48ca14eedb25864098d6bcb52a1",
+    "url": "/poc_convo_quiz/static/media/family11.7f07c48c.mp3"
+  },
+  {
+    "revision": "091834e2b0714b4bca18b28deeb992dc",
+    "url": "/poc_convo_quiz/static/media/family12.091834e2.mp3"
+  },
+  {
+    "revision": "d170766312fbbbd91b0153da8f1d45f8",
+    "url": "/poc_convo_quiz/static/media/family2.d1707663.mp3"
+  },
+  {
+    "revision": "6c9aeae1527506ceae3a602f1c157d5c",
+    "url": "/poc_convo_quiz/static/media/family3.6c9aeae1.mp3"
+  },
+  {
+    "revision": "bdbe423fe05f0aa3b98e89f19d2c9dbd",
+    "url": "/poc_convo_quiz/static/media/family4.bdbe423f.mp3"
+  },
+  {
+    "revision": "97b0ccb39aeaa29054b92dcf0fc19e48",
+    "url": "/poc_convo_quiz/static/media/family5.97b0ccb3.mp3"
+  },
+  {
+    "revision": "7dad05822b1a25ffdbf7f3a08761ff23",
+    "url": "/poc_convo_quiz/static/media/family6.7dad0582.mp3"
+  },
+  {
+    "revision": "712c4b372754f3176ba726533f8ecc8a",
+    "url": "/poc_convo_quiz/static/media/family7.712c4b37.mp3"
+  },
+  {
+    "revision": "6705b6da99ddcffae774d69078d7a0de",
+    "url": "/poc_convo_quiz/static/media/family8.6705b6da.mp3"
+  },
+  {
+    "revision": "b87f6af620f00574fc89a4f6bedb4644",
+    "url": "/poc_convo_quiz/static/media/family9.b87f6af6.mp3"
+  },
+  {
+    "revision": "d0381a84802193afe5114f79976027e6",
+    "url": "/poc_convo_quiz/static/media/food1.d0381a84.mp3"
+  },
+  {
+    "revision": "72217992ae02f771add55bb394ee972a",
+    "url": "/poc_convo_quiz/static/media/food10.72217992.mp3"
+  },
+  {
+    "revision": "39ef3312ccf785ba761f28497094da64",
+    "url": "/poc_convo_quiz/static/media/food11.39ef3312.mp3"
+  },
+  {
+    "revision": "5d66bf9bc068bb66e339666404d6930f",
+    "url": "/poc_convo_quiz/static/media/food12.5d66bf9b.mp3"
+  },
+  {
+    "revision": "0028b8d57509e3ef7545095cd62c1aae",
+    "url": "/poc_convo_quiz/static/media/food2.0028b8d5.mp3"
+  },
+  {
+    "revision": "83387cad59cc39d56725a34256083f39",
+    "url": "/poc_convo_quiz/static/media/food3.83387cad.mp3"
+  },
+  {
+    "revision": "21b8d4368361764fde06363721fbf41a",
+    "url": "/poc_convo_quiz/static/media/food4.21b8d436.mp3"
+  },
+  {
+    "revision": "02047798fd419f8b77d76a28ad5cfc4f",
+    "url": "/poc_convo_quiz/static/media/food5.02047798.mp3"
+  },
+  {
+    "revision": "63a41e37b0a3644b06bdb01535209e6e",
+    "url": "/poc_convo_quiz/static/media/food6.63a41e37.mp3"
+  },
+  {
+    "revision": "607b4a16c89297216f0d9734a194e060",
+    "url": "/poc_convo_quiz/static/media/food7.607b4a16.mp3"
+  },
+  {
+    "revision": "28b5c987d0ab1685457a68b9ea472562",
+    "url": "/poc_convo_quiz/static/media/food8.28b5c987.mp3"
+  },
+  {
+    "revision": "7cbe8e28bc81794a2a456c214b7656c7",
+    "url": "/poc_convo_quiz/static/media/food9.7cbe8e28.mp3"
+  },
+  {
+    "revision": "7b0d5a557543e96bef522b38c2cafac0",
+    "url": "/poc_convo_quiz/static/media/poc.7b0d5a55.jpeg"
+  }
+]);
