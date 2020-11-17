785773433eec6a0c54175109d0fef4b89d0ec894
Rennie Bevineau <rsbevineau@gmail.com>
2020-11-16 21:15:47 -0500

    added the styling for a locked quiz

--- src/Quiz/Quiz.js
+++ src/Quiz/Quiz.js
@@ -2,28 +2,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../AllQuizzes/AllQuizzes.module.css';
+import './Quiz.css';
 
 const Quiz = ({quiz}) => {
-    return ( 
-    <div>
-        <div className={styles.panel}>
-            <img style={{ width: "250px", marginTop: "50px"}}alt="poc-icon"  src="https://i.imgur.com/riiO7TJ.png"/>           
-            
-            <h2>{quiz.quizname}</h2>                
 
-            <p><b>Language Level:</b> {quiz.level}</p>
-            
-            <p><b>Description: </b>{quiz.quizdescription}</p>            
-            
-            <Link className="myButton" to={{
-            pathname:'/StartQuiz',
-            state:{ 
-                quiz: quiz
-            } 
-            }}>Go to Quiz</Link>
-        </div>     
+    if(quiz.locked){
+        return (
+        <div>
+            <div className="lockedquiz">
+                <div class="overlay">Subscribe to Access this Quiz</div>
+                <img style={{ width: "250px", marginTop: "50px"}}alt="poc-icon"  src="https://i.imgur.com/riiO7TJ.png"/>           
+                
+                <h2>{quiz.quizname}</h2>                
 
-    </div> );
+                <p><b>Language Level:</b> {quiz.level}</p>
+                
+                <p><b>Description: </b>{quiz.quizdescription}</p>            
+                
+                <Link className="myButton" to={{
+                pathname:'/StartQuiz',
+                state:{ 
+                    quiz: quiz
+                } 
+                }}>Go to Quiz</Link>
+            </div>  
+        </div>
+        ) 
+    } else {
+       return ( 
+        <div>
+            <div className={styles.panel}>
+                <img style={{ width: "250px", marginTop: "50px"}}alt="poc-icon"  src="https://i.imgur.com/riiO7TJ.png"/>           
+                
+                <h2>{quiz.quizname}</h2>                
+
+                <p><b>Language Level:</b> {quiz.level}</p>
+                
+                <p><b>Description: </b>{quiz.quizdescription}</p>            
+                
+                <Link className="myButton" to={{
+                pathname:'/StartQuiz',
+                state:{ 
+                    quiz: quiz
+                } 
+                }}>Go to Quiz</Link>
+            </div>     
+
+        </div> 
+        ); 
+    }
+    
 }
  
 export default Quiz;
