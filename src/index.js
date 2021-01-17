d1fe7e6646587610e20a8ab8698c38bad4fbd21e
Rennie Bevineau <rsbevineau@gmail.com>
2021-01-16 21:57:37 -0500

    change build script to include CI=false

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
+import { Authpack } from '@authpack/sdk'
+import { AuthpackProvider } from '@authpack/react'
+export const authpack = new Authpack({
+  key: 'wga-client-key-094573d41342e2d35a0552327'
+})
 
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthpackProvider value={authpack}>
+        <App />
+      </AuthpackProvider>
     </BrowserRouter>      
   </React.StrictMode>,
   document.getElementById('root')
